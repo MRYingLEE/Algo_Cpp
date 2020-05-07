58a8dfe75464626c01c156084583e982e3309853
Ying LI <Mr.Ying.LEE@gmail.com>
2020-05-07 10:39:58 +0800

    Update increasing sub-sequences.cpp

--- BetterSolutions/increasing sub-sequences.cpp
+++ BetterSolutions/increasing sub-sequences.cpp
@@ -1,52 +1,49 @@
-﻿#include <iostream>
+// Example program
+#include <iostream>
+#include <string>
+#include <climits>
 #include <vector>
+#include <array>
+#include <chrono>
+#include <functional>
+#include <map>
+
 using namespace std;
+using namespace std::chrono;
 
-
-#define maxN 20 
-#define maxSum 50 
-#define minSum 50 
-#define base 50 
-
-
-// To store the states of DP 
-int dp[maxN][maxSum + minSum];
-
-// Function to return the required count 
-int findCnt(int* arr, int i, int required_sum, int n)
-{
-	if (n == 0) return (required_sum == 0) ? 1 : 0;
-
-	dp[0][0] = 1;
-	for (int j = 1; j < maxSum + minSum; j++)
-		dp[0][j] = 0;
-
-	dp[0][arr[0]]++;
-
-	for (auto k = 1; k < n; k++)
-	{
-		for (int j = 0; j < maxSum + minSum; j++)
-			if ((j - arr[k - 1]) >= 0)
-				dp[k][j] = dp[k - 1][j] + dp[k - 1][j - arr[k - 1]];
-			else
-				dp[k][j] = dp[k - 1][j];
-	}
-
-	auto result = dp[n-1][required_sum];
-
-	return result;
-
-}
 
 // Driver code 
 int main()
 {
-	int arr[] = { 3, 3, 3, 3 };
+	int arr[] = { 1, 2, 10, 9 };
 	int n = sizeof(arr) / sizeof(int);
-	int x = 6;
 
-	cout << "fff" << findCnt(arr, 0, 6, n);
+	int sum_v = 1;
+
+	map<int, int> m;
+	m.emplace(arr[0], 1);
+
+	for (int i = 1; i < n; i++) {
+		int sum_below = 0;
+
+		for (auto j : m) {
+			if (j.first < arr[i])
+				sum_below = sum_below + j.second;
+		}
+
+		auto f = m.find(arr[i]);
+
+		if (f != m.end()) {
+			m[arr[i]] = f->second + sum_below + 1;
+		}
+		else
+			m.emplace(arr[i], sum_below + 1);
+
+		sum_v += sum_below + 1;
+
+	}
+
+	std::cout << sum_v;
 
 	return 0;
 }
-
